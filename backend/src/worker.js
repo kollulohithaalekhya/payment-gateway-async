16182c918d64c56cd6eca77a8837e0825076075c
kollulohithaalekhya <alekhyakollu@gmail.com>
2026-01-14 21:17:08 +0530

    Backend

---
+++ backend/src/worker.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+require('./workers/payment.worker');
+require('./workers/webhook.worker');
+require('./workers/refund.worker');
+
+console.log('👷 Worker started and listening to queues');
+
+
+;
